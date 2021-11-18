7a10d71f8856544b789ab151bde0fe35685b0e8e
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-18 10:53:54 -0800

    tourist_attractions/views.py def home and def details added

--- TouristAttractionswithForms/tourist_attractions/views.py
+++ TouristAttractionswithForms/tourist_attractions/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import State, Attraction
 
-# Create your views here.
+def home(request):
+  all_attractions = Attraction.objects.all()
+  context = {"attractions" : all_attractions}
+  return render(request, 'tourist_attractions/home.html', context)
+
+def details(request, statename):
+  attractions = Attraction.objects.all()
+  context = {"attractions" : attractions, "statename" : statename.replace("-", " ")}
+  return render(request, 'tourist_attractions/details.html', context)
