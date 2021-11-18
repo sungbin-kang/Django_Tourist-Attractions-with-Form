b87d5daadaead4c1f9158a3ae30c9fea697cac3a
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-18 11:04:11 -0800

    main page url path name changed from localhost/tourist_attractions to localhost/

--- TouristAttractionswithForms/TouristAttractionswithForms/urls.py
+++ TouristAttractionswithForms/TouristAttractionswithForms/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("tourist_attractions.urls")),
     path('admin/', admin.site.urls),
-    path("tourist_attractions/", include("tourist_attractions.urls")),
 ]
