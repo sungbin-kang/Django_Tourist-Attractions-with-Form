924919b96993c614df02fe0ab6d8ca58a52a43ac
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-18 11:47:34 -0800

    state and attraction create form classes, urls, and templates added

--- TouristAttractionswithForms/tourist_attractions/urls.py
+++ TouristAttractionswithForms/tourist_attractions/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
   path("", views.home, name="home"),
   path("details/<statename>", views.details, name="details"),
+  path("state/create", views.StateCreate.as_view(), name="state_create"),
+  path("attraction/create", views.AttractionCreate.as_view(), name="attraction_create"),
 ]
