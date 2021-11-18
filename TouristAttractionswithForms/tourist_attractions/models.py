dcb9e38bc489c1fc01138d5628a7451ce4dab9f7
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-18 10:52:19 -0800

    Models added and migrations made

--- TouristAttractionswithForms/tourist_attractions/models.py
+++ TouristAttractionswithForms/tourist_attractions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+  stateName = models.CharField(max_length=50, verbose_name="State Name")
+  stateAbbreviation = models.CharField(max_length=3, verbose_name="State Abbreviation")
+  def __str__(self):
+    return '{}'.format(self.stateName)
+
+class Attraction(models.Model):
+  homeState = models.ForeignKey(State, on_delete=models.CASCADE, verbose_name="Home State")
+  attractionName = models.CharField(max_length=200, verbose_name="Attraction Name")
+  def __str__(self):
+    return '{}'.format(self.attractionName)
